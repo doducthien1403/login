7978f8747f8623c944df0c3c4c979c8e8abbee69
DoDucThien <doducthien1432000@gmail.com>
2022-04-01 10:48:28 +0700

    update main_login

--- lib/main.dart
+++ lib/main.dart
@@ -29,21 +29,137 @@ class Login extends StatelessWidget {
         ),
         actions: <Widget>[
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              print("Click Menu");
+            },
             icon: const Icon(Icons.menu),
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Text("ĐĂNG NHẬP BẰNG TÀI KHOẢN"),
-          TextField(
-            decoration: InputDecoration(hintText: "Tên tài khoản"),
-          ),
-          TextField(
-            decoration: InputDecoration(hintText: "Mật khẩu"),
-          ),
-        ],
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 100,
+            ),
+            const SizedBox(
+              height: 80,
+              child: Text(
+                "ĐĂNG NHẬP BẰNG TÀI KHOẢN",
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue),
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email, color: Colors.blue),
+                  hintText: "Tên tài khoản",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.vpn_key, color: Colors.blue),
+                  hintText: "Mật khẩu",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      print("Click button Quên mật khẩu");
+                    },
+                    child: Row(
+                      children: const <Widget>[
+                        Icon(
+                          Icons.keyboard_arrow_right,
+                          color: Colors.black,
+                        ),
+                        Text(
+                          "Quên mật khẩu",
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Click button Xác nhận");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: const Text(
+                    "Xác nhận",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(
+              height: 100,
+              color: Colors.black87,
+            ),
+            const SizedBox(
+              height: 50,
+              child: Text(
+                "ĐĂNG NHẬP BẰNG",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.facebook,
+                    color: Colors.blue,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 50),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.facebook,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
